cd8231c69377598db8d04cae6f74c2463ec88371
santiv9999 <santiagomava99@hotmail.com>
2021-12-24 02:35:28 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -7,7 +7,7 @@ function obtenerMayor(x, y) {
   // Tu código:
 }
 
-function mayoriaDeEdad(edad) {
+function mayoriaDeEdad("hola") {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
